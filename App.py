84057f64b9eabf0e86c0299d1ce7d6a632a3773b
ajofoyinbo2525-ux <ajofoyinbo2525@gmail.com>
2026-01-03 19:28:23 +0100

    Update App.py

--- App.py
+++ App.py
@@ -1,2 +1,11 @@
 import streamlit as st
 import pandas as pd
+
+st.set_page_config(page_title="Nigeria Mast Planner")
+
+st.title("Nigeria Mast Planner")
+
+df = pd.read_csv("mast_data_final.csv")
+
+st.write("Preview of data:")
+st.dataframe(df.head())
